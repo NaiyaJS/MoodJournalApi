2bd4cb53ee27158c927c8b4742d62a808638125d
NaiyaJS <NaiyaJS04@gmail.com>
2023-11-25 23:40:41 -0500

    first commit

---
+++ src/test/java/com/passionProject/Mood/Journal/MoodJournalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.passionProject.Mood.Journal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoodJournalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
